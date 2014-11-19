9f6ac56869bdcefa4cf183c5fe66ab9a11db9f9f
bert hubert <bert.hubert@netherlabs.nl>
2014-11-19 10:06:22 +0100

    add some documentation, plus coincident with difference

--- coincident.py
+++ coincident.py
@@ -1,6 +1,7 @@
 import sys, random, unittest
 
 def findCoincident(a, b):
+    """Find elements in a and b that are identical at the same position"""
     ret=[]
     for x in xrange(0, min(len(a), len(b))):
         if(a[x]==b[x]):
@@ -8,7 +9,18 @@ def findCoincident(a, b):
 
     return ret
 
+def findCoincidentDiff(a, b, diff):
+    """Find elements in a and b that differ at most diff at the same position"""
+    ret=[]
+    for x in xrange(0, min(len(a), len(b))):
+        if(a[x] - b[x] < diff):
+            ret.append(x)
+
+    return ret
+
+
 def populateLists(a,b):
+    """Fill up two lists with 100000 random elements"""
     for x in range(0, 100000):
         a.append(random.randrange(65536))
         b.append(random.randrange(65536))
@@ -25,6 +37,10 @@ class TestCoincident(unittest.TestCase):
     def test_allMatches(self):
         res = findCoincident([1,2,3], [1,2,3])
         self.assertTrue(res==[0,1,2])
+
+    def test_diff(self):
+        res = findCoincidentDiff([9,9,9], [0,1,10], 1)
+        self.assertTrue(res==[2])
 
     def test_diffLength(self):
         res = findCoincident([1,2], [1,2,3,4])
